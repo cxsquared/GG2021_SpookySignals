e25a0733bedb4de00aa1efc6075a52c0e0641e4b
Travis Faas <travis.faas@gmail.com>
2021-01-30 14:11:09 -0500

    The tween dont tween

--- src/objects/GameMap.hx
+++ src/objects/GameMap.hx
@@ -1,16 +1,38 @@
 package objects;
 
+import h2d.Bitmap;
 import h2d.Object;
+import motion.Actuate;
 
 class GameMap extends h2d.Object {
 
+  var playerChar : h2d.Bitmap;
+
 	public function new(parent : h2d.Object) {
       super(parent);
+
+      //position map
+      this.y = 200;
+
 
       //image background
       var tile = hxd.Res.map.toTile();
       var bmp = new h2d.Bitmap(tile, this);
 
-      this.y = 200;
+      //player character
+      playerChar = new h2d.Bitmap( hxd.Res.pmarker.toTile(), this);
+
+      //
+      var interaction = new h2d.Interactive(800, 600, this);
+
+      //move char
+      interaction.onClick =  function(event:hxd.Event) {
+
+        playerChar.x = 1;
+        Actuate.tween(playerChar, 10, { x: 200, y: 200, posChange: true });
+
+        trace(playerChar.x, playerChar.y);
+
+      }
     }
 }
