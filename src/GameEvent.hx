d1875d8113f2feebd8761e91280c2f57066cada7
Cody Claborn <cxsquared@gmail.com>
2021-01-29 22:30:40 -0500

    Added event listener and firing events

--- src/GameEvent.hx
+++ src/GameEvent.hx
@@ -1,21 +1,20 @@
 import h2d.col.Point;
 
 class GameEvent {
-	var id:String;
-	var type:EventType = Event;
-	var text:String;
-	var audioFile:String;
-	var dependOn:Array<String>;
-	var freq:Float;
-	var location:Point;
-
+	public var id:String;
+	public var type:EventType = Event;
+	public var text:String;
+	public var audioFile:String;
+	public var dependsOn:Array<String>;
+	public var freq:Float;
+	public var location:Point;
 	public var time:Int; // minutes (probably from 0)
 
 	public function new(id:String, text:String, audioFile:String, dependsOn:Array<String>, time:Int) {
 		this.id = id;
 		this.text = text;
 		this.audioFile = audioFile;
-		this.dependOn = dependsOn;
+		this.dependsOn = dependsOn;
 		this.time = time;
 	}
 
--- src/Main.hx
+++ src/Main.hx
@@ -2,17 +2,36 @@ import hxd.res.DefaultFont;
 import h2d.Text;
 
 class Main extends hxd.App {
+	var tf:Text;
+
 	override function init() {
-		var tf = new h2d.Text(DefaultFont.get(), s2d);
+		tf = new h2d.Text(DefaultFont.get(), s2d);
 		tf.text = "Hello World!";
 
 		EventController.instance.loadEvents();
+		EventController.instance.setSpeed(1);
+		EventController.instance.registerEventListener(onGameEvent);
+		hxd.Window.getInstance().addEventTarget(onEvent);
 	}
 
 	override function update(dt:Float) {
 		super.update(dt);
 
 		EventController.instance.update(dt);
+	}
+
+	function onGameEvent(event:GameEvent):Void {
+		tf.text = event.text;
+		EventController.instance.setSpeed(0);
+	}
+
+	function onEvent(event:hxd.Event) {
+		switch (event.kind) {
+			case EPush:
+				if (event.button == 0)
+					EventController.instance.setSpeed(1);
+			case _:
+		}
 	}
 
 	static function main() {
